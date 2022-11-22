d93415939a27c1c5b43942a1c129485d1cdd2d2c
Konstantin <k.podyganov@mail.ru>
2022-11-22 19:00:39 +0300

    Added server auth

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
-import * as express from "express";
-import * as cors from "cors";
-import * as bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import express from "express";
+import cors from "cors";
+import router from "./routes";
+import sequelize from "./config/database";
+
+const PORT = 5000;
 
 const app = express();
-const port = 4001;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use("/api", router);
 
-app.get("/", (req, res, next) => {
-  res.json("Hello world");
-});
+const start = async () => {
+  try {
+    await sequelize.sync({
+      force: true,
+      logging: false,
+    });
 
-app.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
-});
+    app.listen(PORT, () => console.log(`Server started on PORT=${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
